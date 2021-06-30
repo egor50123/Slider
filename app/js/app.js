31ee7dac3cce4138b42d96f49d936283dff69a2a
Egor <yatskovskiy2001@gmail.com>
2021-06-30 15:01:13 +0300

    часть нижней части и слайдер изображения

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+import {Swiper, Parallax, Mousewheel} from 'swiper'
+Swiper.use( Parallax, Mousewheel)
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperIMG = new Swiper('.slider-img', {
+    loop: false,
+    speed: 2400,
+    parallax: true,
+    mousewheel: {
+      invert: false
+    }
+  })
+})
