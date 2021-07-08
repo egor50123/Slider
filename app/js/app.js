31d775064eb8ae5e411977a986ccb6f3ab79d367
Egor <yatskovskiy2001@gmail.com>
2021-07-08 12:26:14 +0300

    Сделана оставшаяся анимация

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,21 @@
 import { Swiper, Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation } from 'swiper'
 Swiper.use( [Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation] )
 
+import { gsap, Power2 } from 'gsap'
+
 document.addEventListener('DOMContentLoaded', () => {
 
   const swiperIMG = new Swiper('.slider-img', {
     loop: false,
     speed: 2400,
     parallax: true,
+    pagination: {
+      el: ".slider-counter__total",
+      type: 'custom',
+      renderCustom: function (swiper, current, total) {
+        return `0${total}`
+      }
+    }
   })
 
   const swiperText = new Swiper('.slider-text', {
@@ -31,4 +40,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
   swiperIMG.controller.control = swiperText
   swiperText.controller.control = swiperIMG
+
+  let gear = document.querySelector('.slider-gear')
+
+  swiperText.on('slideNextTransitionStart', function() {
+    gsap.to(gear, 2.8, {
+      rotation: '+=40',
+      ease: Power2.easeOut
+    })
+  })
+
+  swiperText.on('slidePrevTransitionStart', function() {
+    gsap.to(gear, 2.8, {
+      rotation: '-=40',
+      ease: Power2.easeOut
+    })
+  })
+
+  let curnum = document.querySelector('.slider-counter__current');
+  let pagecur = document.querySelector('.slider-number__num');
+
+  swiperText.on('slideChange', function(){
+    let ind = swiperText.realIndex + 1
+    gsap.to(curnum, .2, {
+      force3D: true,
+      y: -10,
+      opacity: 0,
+      ease: Power2.easeOut,
+      onComplete: function() {
+        gsap.to(curnum, .1, {
+          force3D: true,
+          y: 10
+        })
+        curnum.innerHTML = `0${ind}`,
+        pagecur.innerHTML = `0${ind}`
+      }
+    })
+
+    gsap.to(curnum, .2, {
+      force3D: true,
+      y: 0,
+      opacity: 1,
+      ease: Power2.easeOut,
+      delay: .3
+    })
+  })
 })
